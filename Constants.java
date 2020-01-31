c9af050b07f20e9359aafda1badd3d695a90dca5
LoneFalconer <49881529+LoneFalconer@users.noreply.github.com>
2020-01-30 16:54:46 -0800

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,22 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+
+
+
+
+public final class Constants {
+
+public static int leftFrontDriveMotor = 2;
+public static int leftRearDriveMotor = 3;
+public static int rightFrontDriveMotor = 1;
+public static int rightRearDriveMotor = 4;
+
+
+}
